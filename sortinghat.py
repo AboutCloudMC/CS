99d8e69bbab70bfc833134703911d483f479f6a8
AboutCloudMC <aboutcloudmc@gmail.com>
2024-10-09 12:47:22 +0100

    Finish sorting hat algorithm

--- sortinghat.py
+++ sortinghat.py
@@ -3,12 +3,22 @@ import json
 with open("questions.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
-points = dict(G=0, H=0, R=0, S=0)
+points = dict(Gryffindor=0, Hufflepuff=0, Ravenclaw=0, Slytherin=0)
 
 for question in data["questions"]:
-    print(question)
     answer = input(question["question"] + "\n > ")
     if answer.lower() == "yes":
         for point in question["fittingHouse"]:
-            print(point)
             points[point] += 1
+
+highest = ""
+if points["Gryffindor"] > points["Hufflepuff"]:
+    highest = "Gryffindor"
+else:
+    highest = "Hufflepuff"
+if points[highest] < points["Ravenclaw"]:
+    highest = "Ravenclaw"
+if points[highest] < points["Slytherin"]:
+    highest = "Slytherin"
+
+print("\n You belong to " + highest + "! \n")
