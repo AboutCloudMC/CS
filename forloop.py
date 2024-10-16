f5bc850158d1705d9aba42977401d4c6e0261397
AboutCloudMC <aboutcloudmc@gmail.com>
2024-10-16 12:55:40 +0100

    Comple loop exercises

--- forloop.py
+++ forloop.py
@@ -1,10 +1,10 @@
-#8. Ask the user to enter their forename, take each character in the name and move the value of the character 2 places to the right - output the new name. i.e. "CAT" = "ECV"  (Look up ASCII and these functions chr() and ord()
-#9. Ask the user to enter the name the algorithm above has outputted and then return the original name to the user.
-
-name = input("Please enter your forename: ")
-nameDec = [] 
-
+name = input("Please enter your forename: ").lower()
+newName = ""
 for i in range(len(name)):
-    nameDec[i] = ord(name[i])
-
-print(nameDec)
+    newChr = ord(name[i])-2
+    print(newChr)
+    if newChr < ord("a"):
+        newChr += 26
+    
+    newName += chr(newChr)
+print(newName)
